f5bcc05542074fa60bb563a86624b8b64749fed4
jmaho24 <jmaho24@wgu.edu>
2025-05-08 23:34:24 -0400

    Test

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ package_1 = Package("23","22 Freedom lane","Salt Lake City", "Utah","00001","Tue
 package_2 = Package("23","22 Freedom lane","Salt Lake City", "Utah","00001","Tuesday 5PM","23KG","ready for delivery")
 print(package_1.__str__())
 print(package_2.__str__())
+print(package_2.__str__())
